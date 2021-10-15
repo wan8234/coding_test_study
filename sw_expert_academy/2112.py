f358210d7b2f8b9c2815251d4d47fa6496bf9ab3
wan8234 <wan8234@naver.com>
2021-10-15 19:54:36 +0900

    sw_expert_academy 2112.py practice

---
+++ sw_expert_academy/2112.py
@@ -0,0 +1,66 @@
+# 세로방향 셀 K개 이상 이어야 검사 통과
+# 약품 투여 시 가로줄 모두 변함
+# 두께 D, 가로크기 W, 합격기준 K
+
+def check(cells, D, W, K):
+    for j in range(W):
+        count = 1
+        before = cells[0][j]
+        for i in range(1, D):
+            if before == cells[i][j]:
+                count += 1
+            else:
+                count = 1
+                before = cells[i][j]
+
+            if count >= K:
+                break
+        else:
+            return False
+    return True
+
+def inject(cells, D, W, K, current, count):
+    global result
+    if current == D:
+        temp = check(cells, D, W, K)
+        if temp:            
+            result = min(result, count)        
+        return
+
+    if count >= result:
+        return
+
+    temp_row = cells[current]
+
+    cells[current] = [0] * W    
+    inject(cells, D, W, K, current + 1, count + 1)
+
+    cells[current] = [1] * W
+    inject(cells, D, W, K, current + 1, count + 1)
+
+    cells[current] = temp_row
+    inject(cells, D, W, K, current + 1, count)
+
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    D, W, K = map(int, input().split())
+    cells = []
+    result = int(1e9)
+    for _ in range(D):
+        cells.append(list(map(int, input().split())))
+    
+    if K == 1:
+        result = 0
+        print('#{} {}'.format(test_case, result))
+        continue
+
+    value = check(cells, D, W, K)
+
+    if not value:
+        inject(cells, D, W, K, 0, 0)
+    else:
+        result = 0
+
+    print('#{} {}'.format(test_case, result))
