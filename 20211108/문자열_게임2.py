4f9e50e862bb328706d5e1c59c00cb70e627581e
SeongEun <55864257+nkrang@users.noreply.github.com>
2021-11-08 20:08:10 +0900

    [ADD] 1108

--- 20211108/문자열_게임2.py
+++ 20211108/문자열_게임2.py
@@ -9,6 +9,9 @@ for _ in range(T):
 
     K = int(input())
 
+    #딕셔너리 
+    #키: 알파벳 / 값: 인덱스 배열
+    #알파벳이 인덱스 몇번에 있는지 저장
     for i in range(len(s)):
         if s[i] in dic:
             dic[s[i]].append(i)
@@ -22,13 +25,18 @@ for _ in range(T):
     no = False
     for x in dic:
         arr = dic[x]
+        #인덱스 개수가 K개 이상이면
         if len(arr) >= K:
             no = True
+            #구해야할 것 1. K개를 포함하는 가장 짧은 / 2. K개를 포함하고 앞 뒤가 해당 문자로 같은 가장 긴
+            #둘 다 K개를 포함하면서 앞뒤에 위치해야함
             for i in range(len(arr) - K+1):
+                #K개를 포함하도록 하면서 길이 구하기
                 temp = arr[i + K -1] - arr[i] + 1
                 mini = min(mini, temp)
                 maxi = max(maxi, temp)
     
+    #K개 이상인게 하나도 없으면
     if not no:
         print(-1)
         continue
