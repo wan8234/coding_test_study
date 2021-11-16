8e9f04caf466d2b2941a02a3f115915100bfb481
wan8234 <wan8234@naver.com>
2021-11-16 15:34:45 +0900

    boj_12969.py practice

--- 2111/211118_boj_12969.py
+++ 2111/211118_boj_12969.py
@@ -36,3 +36,31 @@ def dfs(a, b, index, count):
 
 dfs(0, 0, 0, 0)
 print(-1)
+
+#### ANOTHER ####
+
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+dp = [[[False for _ in range(N + 1)] for _ in range(N + 1)] for _ in range(N * (N - 1) // 2 + 1)]
+
+def dfs(count, a, b, string):
+    if len(string) == N:
+        if count == K:
+            print(string)
+            exit(0)
+        return
+    
+    if dp[count][a][b]:
+        return
+    
+    dp[count][a][b] = True
+
+    dfs(count, a + 1, b, string + 'A')
+    dfs(count + a, a, b + 1, string + 'B')
+    dfs(count + a + b, a, b, string + 'C')
+    return
+
+dfs(0, 0, 0, '')
+print(-1)
