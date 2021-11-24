44cc16ec2e29ebd37fe0aaf20d94e04500fabb86
wan8234 <wan8234@naver.com>
2021-11-25 00:37:25 +0900

    boj_1300.py practice

---
+++ 2111/211125_boj_1300.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+K = int(input())
+
+start = 1
+end = N * N
+
+while start <= end:
+    mid = (start + end) // 2
+
+    count = 0
+    for i in range(1, N + 1):
+        count += min(mid//i, N)
+    
+    if count >= K:
+        end = mid - 1
+    else:
+        start = mid + 1
+
+print(start)
