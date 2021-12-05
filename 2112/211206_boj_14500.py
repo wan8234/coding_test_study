8ed6c488695223bd23ecf5454de23661ff70f071
wan8234 <wan8234@naver.com>
2021-12-05 20:18:13 +0900

    boj_14500.py practice

--- 2112/211206_boj_14500.py
+++ 2112/211206_boj_14500.py
@@ -5,25 +5,25 @@ dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
 def dfs(x, y, depth, total):
-	global max_value
-	if total + maxi * (4 - depth) <= max_value:
-		return
-	if depth >= 4:
-		max_value = max(max_value, total)
-		return
-	else:
-		for i in range(4):
-			nx, ny = x + dx[i], y + dy[i]
-			
-			if 0 <= nx < N and 0 <= ny < M and visit[nx][ny] == 0:
-				if depth == 2:
-					visit[nx][ny] = 1
-					dfs(x, y, depth + 1, total + board[nx][ny])
-					visit[nx][ny] = 0
-				
-				visit[nx][ny] = 1
-				dfs(nx, ny, depth + 1, total + board[nx][ny])
-				visit[nx][ny] = 0
+    global max_value
+    if total + maxi * (4 - depth) <= max_value:
+        return
+    if depth >= 4:
+        max_value = max(max_value, total)
+        return
+    else:
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            
+            if 0 <= nx < N and 0 <= ny < M and visit[nx][ny] == 0:
+                if depth == 2:
+                    visit[nx][ny] = 1
+                    dfs(x, y, depth + 1, total + board[nx][ny])
+                    visit[nx][ny] = 0
+                
+                visit[nx][ny] = 1
+                dfs(nx, ny, depth + 1, total + board[nx][ny])
+                visit[nx][ny] = 0
 
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
@@ -34,9 +34,9 @@ max_value = 0
 visit = [[0] * M for _ in range(N)]
 
 for i in range(N):
-	for j in range(M):
-		visit[i][j] = 1
-		dfs(i, j, 1, board[i][j])
-		visit[i][j] = 0
+    for j in range(M):
+        visit[i][j] = 1
+        dfs(i, j, 1, board[i][j])
+        visit[i][j] = 0
 
-print(max_value)	
+print(max_value)    
