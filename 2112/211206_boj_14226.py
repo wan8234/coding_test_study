06e8fc09b58be63019bb34db49d09775be44898d
wan8234 <wan8234@naver.com>
2021-12-05 20:17:24 +0900

    boj_14226.py practice

--- 2112/211206_boj_14226.py
+++ 2112/211206_boj_14226.py
@@ -6,27 +6,27 @@ S = int(input())
 
 dp = [[-1] * (S + 1) for _ in range(S + 1)]
 dp[1][0] = 0
-
+    
 def bfs(emo, clip):
-	queue = deque()
-	queue.append((emo, clip))
-	
-	while queue:
-		e, c = queue.popleft()
-		
-		if dp[e][e] == -1:
-			dp[e][e] = dp[e][c] + 1
-			queue.append((e, e))
-		if e + c <= S and dp[e + c][c] == -1:
-			dp[e + c][c] = dp[e][c] + 1
-			queue.append((e + c, c))
-		if e - 1 >= 0 and dp[e - 1][c] == -1:
-			dp[e - 1][c] = dp[e][c] + 1
-			queue.append((e - 1, c))
+    queue = deque()
+    queue.append((emo, clip))
+    
+    while queue:
+        e, c = queue.popleft()
+        
+        if dp[e][e] == -1:
+            dp[e][e] = dp[e][c] + 1
+            queue.append((e, e))
+        if e + c <= S and dp[e + c][c] == -1:
+            dp[e + c][c] = dp[e][c] + 1
+            queue.append((e + c, c))
+        if e - 1 >= 0 and dp[e - 1][c] == -1:
+            dp[e - 1][c] = dp[e][c] + 1
+            queue.append((e - 1, c))
             
 bfs(1, 0)
 answer = int(1e9)
 for i in range(S + 1):
-	if dp[S][i] != -1:
-		answer = min(answer, dp[S][i])
+    if dp[S][i] != -1:
+        answer = min(answer, dp[S][i])
 print(answer)
