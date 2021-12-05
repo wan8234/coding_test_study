5a8f4e93046a99dd9ad9bf310e999387293136e7
wan8234 <wan8234@naver.com>
2021-12-05 20:16:16 +0900

    boj_5582.py practice

--- 2112/211206_boj_5582.py
+++ 2112/211206_boj_5582.py
@@ -10,10 +10,10 @@
 # dp = [[0] * (len(second) + 1) for _ in range(len(first) + 1)]
 
 # for i in range(1, len(first) + 1):
-# 	for j in range(1, len(second) + 1):
-# 		if (first[i - 1] == second[j - 1]):
-# 			dp[i][j] = dp[i - 1][j - 1] + 1
-# 			answer = max(dp[i][j], answer)
+#   for j in range(1, len(second) + 1):
+#       if (first[i - 1] == second[j - 1]):
+#           dp[i][j] = dp[i - 1][j - 1] + 1
+#           answer = max(dp[i][j], answer)
 
 # #answer = max(map(max, dp))
 # print(answer)
@@ -29,13 +29,13 @@ answer = 0
 dp = [0] * (len(second) + 1)
 
 for i in range(len(first)):
-	temp = [0] * (len(second) + 1)
-	for j in range(len(second)):
-		if first[i] == second[j]:
-			temp[j + 1] = dp[j] + 1
+    temp = [0] * (len(second) + 1)
+    for j in range(len(second)):
+        if first[i] == second[j]:
+            temp[j + 1] = dp[j] + 1
 			
-	answer = max(answer, max(temp))
-	dp = temp[:]
+    answer = max(answer, max(temp))
+    dp = temp[:]
 print(answer)
 
 #### SOLVE 2 ####
@@ -49,12 +49,12 @@ answer = 0
 dp = [0] * (len(second) + 1)
 
 for i in range(len(first)):
-	for j in range(len(second) - 1, -1, -1):
-		if first[i] == second[j]:
-			dp[j] = dp[j - 1] + 1
-			if answer < dp[j]:
-				answer = dp[j]
-		else:
-			dp[j] = 0
+    for j in range(len(second) - 1, -1, -1):
+        if first[i] == second[j]:
+            dp[j] = dp[j - 1] + 1
+            if answer < dp[j]:
+                answer = dp[j]
+        else:
+            dp[j] = 0
 			
 print(answer)
