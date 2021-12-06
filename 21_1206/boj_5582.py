5062015ad81780cf2d9b5f442ee66dea8a075e8b
SangHyune546 <48785671+SangHyune546@users.noreply.github.com>
2021-12-06 20:24:31 +0900

    Add files via upload

---
+++ 21_1206/boj_5582.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+
+str1 = str(input().rstrip())
+str2 = str(input().rstrip())
+
+answer = 0
+
+dp=[[0] * (len(str2) + 1) for _ in range(len(str1) + 1)]
+
+for i in range(1, len(str1)+1):
+    for j in range(1, len(str2)+1):
+        if (str1[i-1] == str2[j-1]):
+            dp[i][j] = dp[i-1][j-1] + 1
+            answer = max(dp[i][j], answer)
+
+print(answer)
