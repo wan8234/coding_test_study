5dad55fd3f3b3bd4a21e757822285d1ac043879b
wan8234 <wan8234@naver.com>
2021-12-27 20:18:00 +0900

    boj_1082.py practice

--- 2112/211227_boj_1082.py
+++ 2112/211227_boj_1082.py
@@ -9,33 +9,33 @@ min_cost = min(prices)
 min_num = prices.index(min_cost)
 
 if N == 1:
-	print(0)
-	sys.exit()
-	
+    print(0)
+    sys.exit()
+    
 num = money // min_cost
 pick = [min_num for i in range(num)]
 cost = num * min_cost
 
 def comb(remains, digit):
-	for i in range(digit, -1, -1):
-		if pick[i] != N - 1:
-			for j in range(N - 1, pick[i], -1):
-				current = prices[j] - prices[pick[i]]
-				if current <= remains:
-					pick[i] = j
-					comb(remains - current, digit - 1)
-					return
-	if not any(pick):
-		if not pick:
-			print(0)
-			sys.exit()
-		pick.pop()
-		comb(remains + prices[0], digit - 1)
+    for i in range(digit, -1, -1):
+        if pick[i] != N - 1:
+            for j in range(N - 1, pick[i], -1):
+                current = prices[j] - prices[pick[i]]
+                if current <= remains:
+                    pick[i] = j
+                    comb(remains - current, digit - 1)
+                    return
+    if not any(pick):
+        if not pick:
+            print(0)
+            sys.exit()
+        pick.pop()
+        comb(remains + prices[0], digit - 1)
 
 comb(money - cost, num - 1)
 
 result = 0
 for i in range(len(pick)):
-	result += (10 ** i) * pick[i]
+    result += (10 ** i) * pick[i]
 
 print(result)
