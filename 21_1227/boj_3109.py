04ec523dec646ed474f7b73468208fb58ddf3d7e
SangHyune546 <48785671+SangHyune546@users.noreply.github.com>
2021-12-27 20:37:04 +0900

    Update boj_3109.py

--- 21_1227/boj_3109.py
+++ 21_1227/boj_3109.py
@@ -9,29 +9,27 @@ R, C = map(int,input().split())
 
 graph = [list(map(str,input().rstrip())) for _ in range(R)]
 
-dx = [-1,0,1] # 우상 우 우하
-dy = 1
+dx = 1 
+dy = [-1,0,1] # 우상 우 우하
 
 answer = 0
 
+def dfs(y,x):
+       
+    graph[y][x] = 'x' #visited 배열 대신 맵 자체에 파이프를 표시 (집과 같이 x로)
 
-def dfs(x,y):
-    
-    
-    graph[x][y] = 'x' #visited 배열 대신 맵 자체에 파이프를 표시 (집과 같이 x로)
-
-    if y == C-1: #마지막 열에 도달하면 성공
+    if x == C-1: #마지막 열에 도달하면 성공
         return True
 
 
-    for i in range(len(dx)):
+    for i in range(len(dy)):
 
-        nx = x + dx[i]
-        ny = y + dy
+        nx = x + dx
+        ny = y + dy[i]
 
-        if 0 <= nx < R and 0 <= ny < C and graph[nx][ny] == '.':
+        if 0 <= nx < C and 0 <= ny < R and graph[ny][nx] == '.':
 
-            if dfs(nx,ny): # 마지막 열까지 파이프 설치시 True 반환
+            if dfs(ny,nx): # 마지막 열까지 파이프 설치시 True 반환
                 return True
 
     return False
