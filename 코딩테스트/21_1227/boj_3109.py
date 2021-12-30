a1c9bf26c453808a8cad20deefee84bc89f47d03
SangHyune546 <48785671+SangHyune546@users.noreply.github.com>
2021-12-30 20:14:49 +0900

    Add files via upload

---
+++ 코딩테스트/21_1227/boj_3109.py
@@ -0,0 +1,43 @@
+import sys
+
+input = sys.stdin.readline
+
+
+
+R, C = map(int,input().split())
+
+
+graph = [list(map(str,input().rstrip())) for _ in range(R)]
+
+dx = 1 
+dy = [-1,0,1] # 우상 우 우하
+
+answer = 0
+
+def dfs(y,x):
+       
+    graph[y][x] = 'x' #visited 배열 대신 맵 자체에 파이프를 표시 (집과 같이 x로)
+
+    if x == C-1: #마지막 열에 도달하면 성공
+        return True
+
+
+    for i in range(len(dy)):
+
+        nx = x + dx
+        ny = y + dy[i]
+
+        if 0 <= nx < C and 0 <= ny < R and graph[ny][nx] == '.':
+
+            if dfs(ny,nx): # 마지막 열까지 파이프 설치시 True 반환
+                return True
+
+    return False
+
+
+for i in range(R):
+
+    if dfs(i,0):
+        answer += 1
+
+print(answer)
